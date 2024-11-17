753a44fd86cc004560314445122fae0c5901ba62
Tequ1Lero <elltilero@gmail.com>
2024-11-17 15:39:53 +0300

    17/11/2024

---
+++ modile_1_strings.py
@@ -0,0 +1,12 @@
+example = 'Проекция'
+print(example[0])
+print(example[-1])
+print(example[4:])
+print(example[::-1])
+print(example[1::2])
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
