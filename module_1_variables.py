8596283f456aa65e6c7bcf1e812b4ceaf96477ce
Tequ1Lero <elltilero@gmail.com>
2024-11-17 14:48:31 +0300

    17/11/2024

---
+++ module_1_variables.py
@@ -0,0 +1,5 @@
+homework_qty = 12
+hours_qty = 1.5
+course_name = 'Python'
+time_for_one_homework = 1.5 / 12
+print(course_name+', всего задач:', homework_qty,', затрачено часов:',hours_qty,', среднее время выполнения:',time_for_one_homework,'часа.')
